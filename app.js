98dbddd1d44a05f774c8dd67e103759680b22efc
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-05-05 14:17:59 -0700

    adding HTML content

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const fs = require('fs'); //require the FileSystem Module to be able to create an html file
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-//print arguments one at a time
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1){
-        console.log(profileDataArr[i]);
-    }
+// console.log(name, github);
+// console.log(generatePage(name, github)); 
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
 
-    console.log('====================');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-
-};
-
-printProfileData(profileDataArgs);
 
 
